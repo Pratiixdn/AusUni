8fbaae39e025be6ad65d0ae50e979cc31ef9f78c
Pratiixdn <pokhrelpratik321@gmail.com>
2026-05-09 08:38:57 +0545

    Update with Render deployment config and bug fixes

--- ausuni/settings.py
+++ ausuni/settings.py
@@ -8,11 +8,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-ausuni-change-this-in-production-use-env-variable'
-
-DEBUG = True  # Set to False in production
-
-ALLOWED_HOSTS = ['*']  # Restrict in production
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ausuni-change-this-in-production-use-env-variable')
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -27,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -90,6 +89,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
