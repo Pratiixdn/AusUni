a4dc4234096954334552ea0c27fe5a04a18cbb27
Pratiixdn <140712788+Pratiixdn@users.noreply.github.com>
2026-05-15 12:19:54 +0545

    Update urls.py

--- ausuni/urls.py
+++ ausuni/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from universities.sitemaps import UniversitySitemap, CourseSitemap, StateSitemap
 
@@ -19,6 +20,7 @@ def ads_txt(request):
     return HttpResponse(content, content_type='text/plain') 
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('ads.txt', ads_txt),
     path('admin/', admin.site.urls),
     path('', include('universities.urls')),
